c419bba0a66408e1b2c213d92e3dd15e8068cbae
matenet <mateuszwnek@int.pl>
2020-08-11 12:08:47 +0200

    fixed loosing state after back from add user

--- src/screens/users/usersListHeader/UsersListHeader.jsx
+++ src/screens/users/usersListHeader/UsersListHeader.jsx
@@ -3,15 +3,19 @@ import Typography from '@material-ui/core/Typography';
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import AddIcon from '@material-ui/icons/Add';
+import {Link} from "react-router-dom";
 
 const UsersListHeader = () => {
 
     return (
         <Grid container direction="row" justify="space-between">
             <Typography variant='h5' display={"inline"} >Users</Typography>
-            <Button variant="contained" color="primary" size="small" startIcon={ <AddIcon /> } href="/add-user">
-                Add User
-            </Button>
+            <Link to={"/add-user"} >
+                <Button variant="contained" color="primary" size="small" startIcon={ <AddIcon /> } >
+                    Add User
+                </Button>
+            </Link>
+
         </Grid>
     )
 };
